ddcf6d41eabe1fb1bf55bae1cc9dd990bffa91f1
Tray Harris <traylee1513@yahoo.com>
2019-09-18 20:58:26 -0700

    Project 1 - Hide the icon images

--- MatchinPairsGame/MatchinPairsGame/Form1.cs
+++ MatchinPairsGame/MatchinPairsGame/Form1.cs
@@ -41,7 +41,7 @@ namespace MatchinPairsGame
                 {
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
-
+                    iconLabel.ForeColor = iconLabel.BackColor;
                     icons.RemoveAt(randomNumber);
                 }
             }
