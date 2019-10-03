11ae9ac4ea261d248d63cbaebbc04d71a11fa69c
Tray Harris <traylee1513@yahoo.com>
2019-10-02 19:23:59 -0700

    Project 2 - Adding Button Controls

--- MusicPlayer/MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Player = new AxWMPLib.AxWindowsMediaPlayer();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,23 +46,97 @@
             this.Player.Location = new System.Drawing.Point(-4, -1);
             this.Player.Name = "Player";
             this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
-            this.Player.Size = new System.Drawing.Size(683, 206);
+            this.Player.Size = new System.Drawing.Size(1172, 402);
             this.Player.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(-4, 173);
+            this.textBox1.Location = new System.Drawing.Point(-4, 369);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(113, 32);
             this.textBox1.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.BackColor = System.Drawing.Color.Maroon;
+            this.BrowseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseButton.ForeColor = System.Drawing.Color.White;
+            this.BrowseButton.Location = new System.Drawing.Point(31, 434);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(141, 49);
+            this.BrowseButton.TabIndex = 2;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = false;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.Color.CadetBlue;
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PlayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.ForeColor = System.Drawing.Color.White;
+            this.PlayButton.Location = new System.Drawing.Point(251, 434);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(141, 49);
+            this.PlayButton.TabIndex = 3;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = false;
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.BackColor = System.Drawing.Color.RosyBrown;
+            this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PauseButton.ForeColor = System.Drawing.Color.White;
+            this.PauseButton.Location = new System.Drawing.Point(514, 434);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(141, 49);
+            this.PauseButton.TabIndex = 4;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = false;
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.Maroon;
+            this.StopButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.StopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopButton.ForeColor = System.Drawing.Color.White;
+            this.StopButton.Location = new System.Drawing.Point(719, 434);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(141, 49);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.Maroon;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.ForeColor = System.Drawing.Color.White;
+            this.ExitButton.Location = new System.Drawing.Point(962, 434);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(141, 49);
+            this.ExitButton.TabIndex = 6;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(671, 226);
+            this.ClientSize = new System.Drawing.Size(1168, 515);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.BrowseButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Player);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -74,6 +154,12 @@
 
         private AxWMPLib.AxWindowsMediaPlayer Player;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
