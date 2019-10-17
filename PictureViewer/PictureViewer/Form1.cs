dba08c7ccd8525c60c127dfa24e9c47d13353b0f
Tray Harris <traylee1513@yahoo.com>
2019-10-16 19:01:16 -0700

    Project 5 - Write codef for additional buttons and checkbox

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -19,17 +19,20 @@ namespace PictureViewer
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
 
         private void setBtn_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
         }
 
         private void clearBtn_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void showBtn_Click(object sender, EventArgs e)
@@ -42,7 +45,14 @@ namespace PictureViewer
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if(checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
         }
     }
 }
