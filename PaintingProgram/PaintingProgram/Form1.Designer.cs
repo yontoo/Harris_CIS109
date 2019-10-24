33070c95021daa11e26ba585533fa3aa5d46daab
Tray Harris <traylee1513@yahoo.com>
2019-10-23 18:19:40 -0700

    Project 6 - Designing the interface pt. 2

--- PaintingProgram/PaintingProgram/Form1.Designer.cs
+++ PaintingProgram/PaintingProgram/Form1.Designer.cs
@@ -34,13 +34,15 @@
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveImageAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.redBtn = new System.Windows.Forms.ToolStripButton();
+            this.blueBtn = new System.Windows.Forms.ToolStripButton();
+            this.greenBtn = new System.Windows.Forms.ToolStripButton();
+            this.purpleBtn = new System.Windows.Forms.ToolStripButton();
+            this.limeBtn = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -77,72 +79,83 @@
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
-            this.toolStripButton3,
-            this.toolStripButton4,
-            this.toolStripButton5});
+            this.redBtn,
+            this.blueBtn,
+            this.greenBtn,
+            this.purpleBtn,
+            this.limeBtn});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(800, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // toolStripButton1
+            // redBtn
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton1.ForeColor = System.Drawing.Color.Red;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(31, 22);
-            this.toolStripButton1.Text = "Red";
+            this.redBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.redBtn.ForeColor = System.Drawing.Color.Red;
+            this.redBtn.Image = ((System.Drawing.Image)(resources.GetObject("redBtn.Image")));
+            this.redBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.redBtn.Name = "redBtn";
+            this.redBtn.Size = new System.Drawing.Size(31, 22);
+            this.redBtn.Text = "Red";
             // 
-            // toolStripButton2
+            // blueBtn
             // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton2.ForeColor = System.Drawing.Color.Blue;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(34, 22);
-            this.toolStripButton2.Text = "Blue";
+            this.blueBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.blueBtn.ForeColor = System.Drawing.Color.Blue;
+            this.blueBtn.Image = ((System.Drawing.Image)(resources.GetObject("blueBtn.Image")));
+            this.blueBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.blueBtn.Name = "blueBtn";
+            this.blueBtn.Size = new System.Drawing.Size(34, 22);
+            this.blueBtn.Text = "Blue";
             // 
-            // toolStripButton3
+            // greenBtn
             // 
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton3.ForeColor = System.Drawing.Color.Green;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(42, 22);
-            this.toolStripButton3.Text = "Green";
+            this.greenBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.greenBtn.ForeColor = System.Drawing.Color.Green;
+            this.greenBtn.Image = ((System.Drawing.Image)(resources.GetObject("greenBtn.Image")));
+            this.greenBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.greenBtn.Name = "greenBtn";
+            this.greenBtn.Size = new System.Drawing.Size(42, 22);
+            this.greenBtn.Text = "Green";
             // 
-            // toolStripButton4
+            // purpleBtn
             // 
-            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton4.ForeColor = System.Drawing.Color.Purple;
-            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
-            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(45, 22);
-            this.toolStripButton4.Text = "Purple";
+            this.purpleBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.purpleBtn.ForeColor = System.Drawing.Color.Purple;
+            this.purpleBtn.Image = ((System.Drawing.Image)(resources.GetObject("purpleBtn.Image")));
+            this.purpleBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.purpleBtn.Name = "purpleBtn";
+            this.purpleBtn.Size = new System.Drawing.Size(45, 22);
+            this.purpleBtn.Text = "Purple";
             // 
-            // toolStripButton5
+            // limeBtn
             // 
-            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton5.ForeColor = System.Drawing.Color.LimeGreen;
-            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
-            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(37, 22);
-            this.toolStripButton5.Text = "Lime";
+            this.limeBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.limeBtn.ForeColor = System.Drawing.Color.LimeGreen;
+            this.limeBtn.Image = ((System.Drawing.Image)(resources.GetObject("limeBtn.Image")));
+            this.limeBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.limeBtn.Name = "limeBtn";
+            this.limeBtn.Size = new System.Drawing.Size(37, 22);
+            this.limeBtn.Text = "Lime";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 401);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
@@ -151,6 +164,7 @@
             this.menuStrip1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,11 +177,12 @@
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveImageAsToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
-        private System.Windows.Forms.ToolStripButton toolStripButton3;
-        private System.Windows.Forms.ToolStripButton toolStripButton4;
-        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton redBtn;
+        private System.Windows.Forms.ToolStripButton blueBtn;
+        private System.Windows.Forms.ToolStripButton greenBtn;
+        private System.Windows.Forms.ToolStripButton purpleBtn;
+        private System.Windows.Forms.ToolStripButton limeBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
