ebdd951bb4aca6fc427fbad4e682ee755f936d7b
tharris99 <traylee1513@yahoo.com>
2019-10-30 12:35:38 -0700

    Project 7 - Add components to form

--- CurrencyConverter/CurrencyConverter/Form1.Designer.cs
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.BackColor = System.Drawing.Color.DarkGreen;
+            this.ConvertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertBtn.Location = new System.Drawing.Point(0, -1);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(805, 71);
+            this.ConvertBtn.TabIndex = 0;
+            this.ConvertBtn.Text = "Currency Converter";
+            this.ConvertBtn.UseVisualStyleBackColor = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 76);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(788, 374);
+            this.webBrowser1.TabIndex = 1;
             // 
             // currencyConvert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.ConvertBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -46,6 +69,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
