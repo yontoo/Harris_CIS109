a706dbf9ab27a98c69dfa419cc690a68e8ec078a
Tray Harris <traylee1513@yahoo.com>
2019-11-06 18:47:10 -0800

    Project 11 - Design interface

--- SimpleDigitalClock/SimpleDigitalClock/Form1.Designer.cs
+++ SimpleDigitalClock/SimpleDigitalClock/Form1.Designer.cs
@@ -28,23 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 120F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-28, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(932, 382);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00 : 00 : 00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
             // 
             // clock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(874, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "clock";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Digital Clock";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- SimpleDigitalClock/SimpleDigitalClock/Form1.cs
+++ SimpleDigitalClock/SimpleDigitalClock/Form1.cs
@@ -16,5 +16,6 @@ namespace SimpleDigitalClock
         {
             InitializeComponent();
         }
+
     }
 }
