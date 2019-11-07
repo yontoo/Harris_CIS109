22d65d3661a8af2f0823d85e76c466f750328fb8
Tray Harris <traylee1513@yahoo.com>
2019-11-06 18:43:14 -0800

    Project 11 - Create Form

---
+++ SimpleDigitalClock/SimpleDigitalClock/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleDigitalClock
+{
+    public partial class clock : Form
+    {
+        public clock()
+        {
+            InitializeComponent();
+        }
+    }
+}
