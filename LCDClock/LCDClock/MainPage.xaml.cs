7fae4463e9082c7bb48821d9d1786844b20b8948
tharris99 <traylee1513@yahoo.com>
2019-12-04 17:05:59 -0800

    A commit

--- LCDClock/LCDClock/MainPage.xaml.cs
+++ LCDClock/LCDClock/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
+using Windows.Devices.I2c;
+using Windows.Devices.Enumeration;
+
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +28,39 @@ namespace LCDClock
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
         public MainPage()
         {
             this.InitializeComponent();
+
         }
+
+        public async void I2C()
+        {
+            //I2cDevice lcd = null;
+            
+            //// 0x27 is the I2C device address
+            //var settings = new I2cConnectionSettings(0x27);
+            //// FastMode = 400KHz
+            //settings.BusSpeed = I2cBusSpeed.FastMode;
+
+            //// Create an I2cDevice with the specified I2C settings
+            //var controller = await I2cController.GetDefaultAsync();
+
+            //string query = I2cDevice.GetDeviceSelector();
+            //DeviceInformationCollection deviceInfo = await DeviceInformation.FindAllAsync(query);
+
+            //lcd = await I2cDevice.FromIdAsync(deviceInfo[0].Id, settings);
+
+            //lcd.Write(new byte[] { 0x80, 0x30 });
+
+            //using (I2cDevice device = controller.GetDevice(settings))
+            //{
+            //    byte[] writeBuf = { 0x30 };
+            //    device.Write(writeBuf);
+            //}
+        }
+
+
     }
 }
